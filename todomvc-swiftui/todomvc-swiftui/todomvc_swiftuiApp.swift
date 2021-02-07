72234ff231e52e0ac2ee52695c7e55c1193830e5
ryanzzff <ryanzzff@gmail.com>
2021-02-07 15:42:37 +0800

    create empty project

---
+++ todomvc-swiftui/todomvc-swiftui/todomvc_swiftuiApp.swift
@@ -0,0 +1,20 @@
+//
+//  todomvc_swiftuiApp.swift
+//  todomvc-swiftui
+//
+//  Created by Ryan Lee on 7/2/2021.
+//
+
+import SwiftUI
+
+@main
+struct todomvc_swiftuiApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
